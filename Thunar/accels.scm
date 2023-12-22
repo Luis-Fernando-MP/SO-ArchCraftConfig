51a06e240305e687c8bfa5e58e443718c5f510a3
JuJu <luigfmp@gmail.com>
2023-12-21 20:34:15 -0500

    feat: :tada: Init config :D

---
+++ Thunar/accels.scm
@@ -0,0 +1,9 @@
+; thunar GtkAccelMap rc-file         -*- scheme -*-
+; this file is an automated accelerator map dump
+;
+(gtk_accel_path "<Actions>/ThunarActions/uca-action-1622790717174452-1" "F3")
+(gtk_accel_path "<Actions>/ThunarStandardView/create-folder" "<Shift>m")
+(gtk_accel_path "<Actions>/ThunarActions/uca-action-1581593230771688-1" "F4")
+(gtk_accel_path "<Actions>/ThunarActionManager/open-in-new-tab" "<Shift>n")
+(gtk_accel_path "<Actions>/ThunarWindow/view-menubar" "<Shift>b")
+(gtk_accel_path "<Actions>/ThunarWindow/close-tab" "<Shift>q")
